1a1cbf13384774dd4802503c8b27d6fcd99f3514
Louise Findlay <louise@louisefindlay.com>
2022-11-17 14:43:42 +0000

    Obtained date and saved as JSON string to a JSON file

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 const token = process.env.BOT_TOKEN;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const axios = require("axios");
+const fs = require("fs");
 
 client.once("ready", () => {
     console.info("Ready!");
@@ -26,18 +27,27 @@ client.once("ready", () => {
             data = data.split("\n").slice(1).join("\n");
             // Parse JSON
             data = JSON.parse(data);
-            data.items[0].community.forEach((post) => {
+            const communityPosts = data.items[0].community;
+            const lastTime = JSON.stringify(communityPosts[0].date);
+            console.info(`Date of latest post is: ${lastTime}`);
+            // TODO: Now keep previous time by moving this after send and compare new post date lastTime from API. Only send if newer.
+            fs.writeFile("lastPostTime.json", lastTime, function (err) {
+                if (err) {
+                    console.info(err);
+                }
+            });
+            communityPosts.forEach((post) => {
                 // Send Discord message to channel
                 const postText = post.contentText[0].text;
                 if (post.image) {
                     const imgURL = post.image.thumbnails[5].url;
                     const imgEmbed = new EmbedBuilder()
-                        .setTitle("New YT Image Post")
+                        .setTitle("New YT Image")
                         .setDescription(postText)
                         .setImage(imgURL);
-                    channel.send({
+                    /* channel.send({
                         embeds: [imgEmbed],
-                    });
+                    }); */
                 } else if (post.poll) {
                     let choiceArray = [];
                     post.poll.choices.forEach((choice) => {
@@ -49,16 +59,16 @@ client.once("ready", () => {
                         .setDescription(
                             `**Poll Title:** ${postText} \n**Choices:** \n${choiceArray}\n**Total Votes:** ${post.poll.totalVotes}`
                         );
-                    channel.send({
+                    /* channel.send({
                         embeds: [pollEmbed],
-                    });
+                    }); */
                 } else {
                     const textEmbed = new EmbedBuilder()
                         .setTitle("New YT Post")
                         .setDescription(postText);
-                    channel.send({
+                    /* channel.send({
                         embeds: [textEmbed],
-                    });
+                    }); */
                 }
             });
         })

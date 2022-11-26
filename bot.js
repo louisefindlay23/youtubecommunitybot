ec0933ddedd8d18dce3b3030c58fe4b1e297e1ae
Louise Findlay <louise@louisefindlay.com>
2022-11-26 21:40:36 +0000

    Posts latest post if day is newer

--- bot.js
+++ bot.js
@@ -29,64 +29,84 @@ client.once("ready", () => {
             data = JSON.parse(data);
             const communityPosts = data.items[0].community;
             let newTime = JSON.stringify(communityPosts[0].date);
-            console.info(`Date of latest post is: ${lastTime}`);
-            // TODO: Now keep previous time by moving this after send and compare new post date lastTime from API. Only send if newer.
+            let lastTime = null;
+            // TODO: Now compare new post date lastTime from API. Only send if newer.
             fs.readFile("./lastPostTime.json", "utf8", (err, lastTime) => {
                 if (err) {
                     console.error("File read failed:", err);
                     return;
                 } else {
-                    console.info(
-                        `Date of latest post is: ${JSON.parse(lastTime)}`
-                    );
+                    lastTime = JSON.parse(lastTime);
+                    newTime = JSON.parse(newTime);
+                    console.info(`Date of previous post is: ${lastTime}`);
+                    console.info(`Date of latest post is: ${newTime}`);
+
                     // TODO: Find out how to compare 5 hours ago etc. time difference
-                }
-            });
-            communityPosts.forEach((post) => {
-                // Send Discord message to channel
-                const postText = post.contentText[0].text;
-                if (post.image) {
-                    const imgURL = post.image.thumbnails[5].url;
-                    const imgEmbed = new EmbedBuilder()
-                        .setTitle("New YT Image")
-                        .setDescription(postText)
-                        .setImage(imgURL);
-                    /* channel.send({
-                        embeds: [imgEmbed],
-                    }); */
-                } else if (post.poll) {
-                    let choiceArray = [];
-                    post.poll.choices.forEach((choice) => {
-                        choiceArray.push(choice.text);
-                    });
-                    choiceArray = choiceArray.join("\n");
-                    const pollEmbed = new EmbedBuilder()
-                        .setTitle("New YT Poll")
-                        .setDescription(
-                            `**Poll Title:** ${postText} \n**Choices:** \n${choiceArray}\n**Total Votes:** ${post.poll.totalVotes}`
-                        );
-                    /* channel.send({
-                        embeds: [pollEmbed],
-                    }); */
-                } else {
-                    const textEmbed = new EmbedBuilder()
-                        .setTitle("New YT Post")
-                        .setDescription(postText);
-                    /* channel.send({
-                        embeds: [textEmbed],
-                    }); */
-                }
-                // TODO: Check this updates
-                fs.writeFile("lastPostTime.json", lastTime, function (err) {
-                    if (err) {
-                        console.info(err);
+                    if (lastTime.includes("days")) {
+                        console.info("Days");
+                        lastTime = parseInt(lastTime.split(" ")[0]);
+                        newTime = parseInt(newTime.split(" ")[0]);
+                        if (lastTime > newTime) {
+                            postContent(communityPosts, newTime);
+                        }
                     }
-                });
+                }
             });
         })
         .catch(function (error) {
             console.log(error);
         });
+
+    function postContent(communityPost, newTime) {
+        const post = communityPost[0];
+        // Send Discord message to channel
+        const postText = post.contentText[0].text;
+        console.info(postText);
+        if (post.image) {
+            const imgURL = post.image.thumbnails[5].url;
+            const imgEmbed = new EmbedBuilder()
+                .setTitle("New YT Image")
+                .setDescription(postText)
+                .setImage(imgURL);
+            channel.send({
+                embeds: [imgEmbed],
+            });
+        } else if (post.poll) {
+            let choiceArray = [];
+            post.poll.choices.forEach((choice) => {
+                choiceArray.push(choice.text);
+            });
+            choiceArray = choiceArray.join("\n");
+            const pollEmbed = new EmbedBuilder()
+                .setTitle("New YT Poll")
+                .setDescription(
+                    `**Poll Title:** ${postText} \n**Choices:** \n${choiceArray}\n**Total Votes:** ${post.poll.totalVotes}`
+                );
+            channel.send({
+                embeds: [pollEmbed],
+            });
+        } else {
+            const textEmbed = new EmbedBuilder()
+                .setTitle("New YT Post")
+                .setDescription(postText);
+            channel.send({
+                embeds: [textEmbed],
+            });
+        }
+        // TODO: Check this updates
+        newTime = JSON.stringify(newTime);
+        fs.writeFile(
+            "lastPostTime.json",
+            JSON.stringify(post.date),
+            function (err) {
+                if (err) {
+                    console.info(err);
+                } else {
+                    console.info("Time written");
+                }
+            }
+        );
+    }
 });
 
 client.login(token);
